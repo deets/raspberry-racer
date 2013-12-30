84d3d2629a426bd516b9a9928d1778ad95495f2e
Diez B. Roggisch <deets@web.de>
2013-12-30 17:52:47 +0100

    introduced matrix equality operator.

--- cpp/src/tests/common.cc
+++ cpp/src/tests/common.cc
@@ -1,9 +1,21 @@
 #include "tests/common.hh"
 
+#define NEAR_EPSILON 0.000001
+
 namespace rracer {
   void ASSERT_VECTOR_EQ(const Vector& a, const Vector& b)
   {
-    ASSERT_NEAR(a[0], b[0], 0.000001);
-    ASSERT_NEAR(a[1], b[1], 0.000001);
+    ASSERT_NEAR(a[0], b[0], NEAR_EPSILON);
+    ASSERT_NEAR(a[1], b[1], NEAR_EPSILON);
   }
+
+
+  void ASSERT_MATRIX_EQ(const AffineTransform& a, const AffineTransform& b) {
+    for(int i = 0; i < 3; i++) {
+      for(int j = 0; j < 3; j++) {
+	ASSERT_NEAR(a(i, j), b(i, j), NEAR_EPSILON);
+      }
+    }
+  }
+
 }; // end ns::rracer
--- cpp/src/tests/common.hh
+++ cpp/src/tests/common.hh
@@ -3,4 +3,5 @@
 
 namespace rracer {
   void ASSERT_VECTOR_EQ(const Vector& a, const Vector& b);
+  void ASSERT_MATRIX_EQ(const AffineTransform& a, const AffineTransform& b);
 }; // end ns::rracer
